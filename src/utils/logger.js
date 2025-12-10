7752f91248d013f372588811365f457b7af05653
SAVAGE BOY <itc.kingsavage@gmail.com>
2025-12-10 22:30:39 +0300

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,41 @@
+// src/utils/logger.js
+
+import colors from "./colors.js";
+
+const timestamp = () => {
+    const now = new Date();
+    return now.toISOString().replace("T", " ").split(".")[0];
+};
+
+const logger = {
+    info: (msg) => {
+        console.log(
+            `${colors.cyanText("[INFO]")} ${colors.dim(`[${timestamp()}]`)} ${msg}`
+        );
+    },
+
+    success: (msg) => {
+        console.log(
+            `${colors.greenText("[SUCCESS]")} ${colors.dim(`[${timestamp()}]`)} ${msg}`
+        );
+    },
+
+    warn: (msg) => {
+        console.log(
+            `${colors.yellowText("[WARNING]")} ${colors.dim(`[${timestamp()}]`)} ${msg}`
+        );
+    },
+
+    error: (msg) => {
+        console.log(
+            `${colors.redText("[ERROR]")} ${colors.dim(`[${timestamp()}]`)} ${msg}`
+        );
+    },
+
+    custom: (tag, color, msg) => {
+        const tagStyled = colors.colorize(`[${tag}]`, color);
+        console.log(`${tagStyled} ${colors.dim(`[${timestamp()}]`)} ${msg}`);
+    }
+};
+
+export default logger;

ee2b09fd9980a2d5c7a8d88460f9c16635209d7c
SAVAGE BOY <itc.kingsavage@gmail.com>
2025-12-13 13:14:33 +0300

    Update qr.js

--- src/routes/qr.js
+++ src/routes/qr.js
@@ -3,19 +3,8 @@ import { generateQR } from "../qr/generate.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    await generateQR(req, res);
-  } catch (err) {
-    console.error(err);
-
-    if (!res.headersSent) {
-      res.status(500).json({
-        success: false,
-        error: "Failed to generate QR"
-      });
-    }
-  }
+router.get("/", (req, res) => {
+  generateQR(req, res);
 });
 
 export default router;

909ccfb58b4200226bb16364a958ba9dfd067979
SAVAGE BOY <itc.kingsavage@gmail.com>
2025-12-13 11:04:55 +0300

    Update qr.js

--- src/routes/qr.js
+++ src/routes/qr.js
@@ -7,6 +7,7 @@ router.get("/", async (req, res) => {
   try {
     await generateQR(res);
   } catch (err) {
+    console.error(err);
     res.status(500).json({
       success: false,
       error: "Failed to generate QR"
